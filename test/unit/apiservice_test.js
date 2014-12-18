d72667cab7a424bef109d43ca035cc5b07f144d7
Chelaru Artur <sirmarcus1993@gmail.com>
2014-12-18 12:21:08 +0200

    identations

--- test/unit/apiservice_test.js
+++ test/unit/apiservice_test.js
@@ -3,27 +3,24 @@ describe('Unit: apiService', function() {
   beforeEach(module('app'));
 	  beforeEach(module('appServices'));
 	  
-	  beforeEach(inject(function(_apiService_, _$http_ , $injector){
-	  	$httpBackend = $injector.get('$httpBackend');	  	
-	  	$http = _$http_;
-	  	apiService = _apiService_;
+		  beforeEach(inject(function(_apiService_, _$http_ , $injector){
+			$httpBackend = $injector.get('$httpBackend');
+			$http = _$http_;
+			apiService = _apiService_;
 
-	  	$httpBackend.when('JSONP','http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=wvnw24xfn5msywmrbv4c59v5&callback=JSON_CALLBACK&page=1&page_limit=30&q=spider')
-	  			.respond({movies:30, q : 'spider'});
-	  }));
+			$httpBackend.whenJSONP('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=wvnw24xfn5msywmrbv4c59v5&callback=JSON_CALLBACK&page=1&page_limit=30&q=spider')
+				.respond ({movies:30, q : 'spider'});
+		  }));
 
-	  afterEach(function() {
-     $httpBackend.verifyNoOutstandingExpectation();
-     $httpBackend.verifyNoOutstandingRequest();
-   });
+		  afterEach(function() {
+			 $httpBackend.verifyNoOutstandingExpectation();
+			 $httpBackend.verifyNoOutstandingRequest();
+	   });
 
 	  it("should check if apiService works",function(){
-
 			apiService.searchMovies('spider',30,1).success(function(data){
 				expect(data).toBeDefined();
-				console.log(data);
 			});
 	  		$httpBackend.flush();
-
 	  });
 })
